8fda7fe65c34f1eeb890fda0ce71357db786fadf
Varun S Athreya <varun.sathreya@gmail.com>
2020-07-05 20:34:55 +0530

    made search page preserve state

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -13,7 +13,8 @@ class Search extends StatefulWidget {
   _SearchState createState() => _SearchState();
 }
 
-class _SearchState extends State<Search> {
+class _SearchState extends State<Search>
+    with AutomaticKeepAliveClientMixin<Search> {
   TextEditingController searchController = TextEditingController();
   Future<QuerySnapshot> searchResultsFuture;
 
@@ -98,8 +99,12 @@ class _SearchState extends State<Search> {
     );
   }
 
+  bool get wantKeepAlive => true;
+
   @override
   Widget build(BuildContext context) {
+    super.build(context);
+
     return Scaffold(
       appBar: buildSearchField(),
       body:
