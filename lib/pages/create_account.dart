f5498904b99a46fc63435855f68e477736de8d86
Varun S Athreya <varun.sathreya@gmail.com>
2020-10-23 00:19:59 +0530

    Fixed issues

--- lib/pages/create_account.dart
+++ lib/pages/create_account.dart
@@ -9,31 +9,31 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccountState extends State<CreateAccount> {
-
   final _scafflodkey = GlobalKey<ScaffoldState>();
-  final _formkey = GlobalKey <FormState> ();
+  final _formkey = GlobalKey<FormState>();
   String username;
 
-  submit(){
+  submit() {
     final form = _formkey.currentState;
 
-    if(form.validate()){
+    if (form.validate()) {
       form.save();
-      SnackBar snackbar = SnackBar(content: Text('Welcome $username!'),);
+      SnackBar snackbar = SnackBar(
+        content: Text('Welcome $username!'),
+      );
       _scafflodkey.currentState.showSnackBar(snackbar);
-      Timer(Duration(seconds: 2), (){
+      Timer(Duration(seconds: 2), () {
         Navigator.pop(context, username);
       });
-
     }
-
   }
 
   @override
   Widget build(BuildContext parentContext) {
     return Scaffold(
       key: _scafflodkey,
-      appBar: header(context, titleText: 'Set up your profile', removeBackButton: true),
+      appBar: header(context,
+          titleText: 'Set up your profile', removeBackButton: true),
       body: ListView(
         children: <Widget>[
           Container(
@@ -55,14 +55,13 @@ class _CreateAccountState extends State<CreateAccount> {
                   child: Container(
                     child: Form(
                       key: _formkey,
-                      autovalidate: true,
                       child: TextFormField(
-                        validator: (val){
-                          if(val.trim().length < 3 || val.isEmpty ){
+                        validator: (val) {
+                          if (val.trim().length < 3 || val.isEmpty) {
                             return 'Username too short';
-                          }else if(val.trim().length > 12){
+                          } else if (val.trim().length > 12) {
                             return 'Username too long';
-                          }else
+                          } else
                             return null;
                         },
                         onSaved: (val) => username = val,
--- lib/widgets/custom_image.dart
+++ lib/widgets/custom_image.dart
@@ -1,6 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import '../widgets/progress.dart';
 
 Widget cachedNetworkImage(String mediaUrl) {
   return CachedNetworkImage(
